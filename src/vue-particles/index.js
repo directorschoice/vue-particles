37ecf79361d1d25424d60760b0c248664244ce64
Trey Caliva <trey.caliva@directorschoice.travel>
2019-06-27 16:52:43 -0500

    Updated src/vue-particles/index.js

--- src/vue-particles/index.js
+++ src/vue-particles/index.js
@@ -3,7 +3,7 @@ import particles from './vue-particles.vue'
 
 const VueParticles = {
 
-    install: (Vue, options) {
+    install (Vue, options) {
         Vue.component('vue-particles', particles)
     }
 
